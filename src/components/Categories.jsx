4a41576f4e83c82a1840254de2c5af20317ba3bb
MAhmadS <bscs22078@itu.edu.pk>
2024-10-07 22:35:32 +0500

    add useEffect hooks

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import Tabs from "./UI components/Tabs";
 
-const Categories = () => {
-    const [selCategory, setSelCategory] = useState("");
+const Categories = (props) => {
   const tabItems = [
     { name: "Icons", img: "/imgs/cat_icons/1.webp" },
     { name: "Rooms", img: "/imgs/cat_icons/2.jpg" },
@@ -10,7 +9,7 @@ const Categories = () => {
     { name: "Amazing views", img: "/imgs/cat_icons/4.jpg" },
     { name: "Historic homes", img: "/imgs/cat_icons/5.jpg" },
   ];
-  return <Tabs tabItems={tabItems} setSelCategory = {setSelCategory} />;
+  return <Tabs tabItems={tabItems} setSelCategory = {props.setSelCategory} />;
 };
 
 export default Categories;
